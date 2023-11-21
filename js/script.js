7badbbd11cd21a1d3560d329e95ace52f6be9828
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-21 14:10:01 +0100

    add: email generator

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-
+            emailList: []
         };
     },
     methods: {
-
+        generateEmailList: function () {
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((resp) => {
+                        this.emailList.push(resp.data.response)
+                    })
+            }
+        }
     },
 }).mount('#app');
+
