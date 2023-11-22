6fe05edd04d78a745a704f50d04df4ee6d7f6392
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-22 10:23:47 +0100

    add: loading

--- js/script.js
+++ js/script.js
@@ -3,19 +3,26 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            emailList: []
+            emailList: [],
+            isLoading: false,
         };
     },
     methods: {
         generateEmailList: function () {
-            for (let i = 0; i < 10; i++) {
+            this.emailList = [];
+            this.isLoading = true;
+            for (let i = 0; i < 10; i++) { //ciclo for perchè così invece di 1 risp me ne da 10, mi stampa 10 indirizzi email
                 axios
+                    //chiamata con get e scriviamo il nostro indirizzo api 
                     .get("https://flynn.boolean.careers/exercises/api/random/mail")
                     .then((resp) => {
-                        this.emailList.push(resp.data.response)
-                    })
+                        this.emailList.push(resp.data.response);
+                        if (this.emailList.length === 10) {
+                            this.isLoading = false;
+                        }
+                    });
             }
-        }
+        },
     },
 }).mount('#app');
 
